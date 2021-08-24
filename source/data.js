8b982aa85eeefafd5d11fe1e5229bc8f81400c89
hamza2512 <hamzashafiq259@gmail.com>
2021-08-24 12:01:14 +0500

    just commit

---
+++ source/data.js
@@ -0,0 +1,55 @@
+//Dummy data 
+export const CONTENT =[
+    {
+  
+      isExpanded : false,
+      category_name:'Item 1',
+      subcategory : [
+        {id :1, val :'sub 1'},
+        {id :2, val :'sub 2'},
+        {id :3, val :'sub 3'},
+        {id :4, val :'sub 4'}
+      ]
+    },
+    {
+  
+      isExpanded : false,
+      category_name:'Item 2',
+      subcategory : [
+        {id :5, val :'sub 5'},
+        {id :6, val :'sub 6'},
+        {id :7, val :'sub 7'},
+        {id :8, val :'sub 8'}
+      ]
+    },
+    {
+  
+      isExpanded : false,
+      category_name:'Item 3',
+      subcategory : [
+        {id :9, val :'sub 9'},
+        {id :10, val :'sub 10'},
+        {id :11, val :'sub 11'},
+        {id :12, val :'sub 12'}
+      ]
+    },
+    {
+  
+      isExpanded : false,
+      category_name:'Item 4',
+      subcategory : [
+        {id :13, val :'sub 13'},
+        {id :14, val :'sub 14'},
+        {id :15, val :'sub 15'},
+      
+      ]
+    },
+    {
+      isExpanded : false,
+      category_name:'Item 5',
+      subcategory : [   
+        {id :16, val :'sub 16'},
+        {id :17, val :'sub 17'}
+      ]
+    },
+  ];
